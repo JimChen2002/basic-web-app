c97ebd47bee8b7ff23c92062e8cdbc55d2ef9aa1
Jim Chen <yijunc@andrew.cmu.edu>
2023-03-14 16:01:35 -0400

    q2

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -2,6 +2,11 @@ export default function QueryProcessor(query: string): string {
     if (query === "What is 12 plus 77?") {
         return "89";
     }
+    if (query.includes("plus")) {
+        var numbers = query.match(/\d+/);
+        if(numbers==null) numbers = ["1","2"];
+        return (Number(numbers[0])+Number(numbers[1])).toString();
+    }
     if (query === "What is your name?") {
         return "Great";
     }
