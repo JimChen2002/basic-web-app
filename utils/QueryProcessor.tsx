40fc1f0fb4224c1737e9b53b3a818574654bdf4b
Jim Chen <yijunc@andrew.cmu.edu>
2023-03-14 16:06:57 -0400

    q2

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -1,11 +1,13 @@
 export default function QueryProcessor(query: string): string {
     if (query.includes("Which of the following numbers is the largest")) {
-        var numbers = query.match(/\d+/);
+        var pattern = /\d+/g;
+        var numbers = query.match(pattern);
         if(numbers==null) numbers = ["1","2","3"];
         return (Math.max(Number(numbers[0]),Number(numbers[1]),Number(numbers[2]))).toString();
     }
     if (query.includes("plus")) {
-        var numbers = query.match(/\d+/);
+        var pattern = /\d+/g;
+        var numbers = query.match(pattern);
         if(numbers==null) numbers = ["1","2"];
         return (Number(numbers[0])+Number(numbers[1])).toString();
     }
